01472b3db74d9c23f8fffa3bef0542ea18c2619a
dreamingdodo <81778299+dreamingdodo@users.noreply.github.com>
2024-09-22 22:08:42 +0200

    Update yolo.ps1

--- yolo.ps1
+++ yolo.ps1
@@ -1,27 +1,55 @@
-# Install git for windows
+Add-Type -AssemblyName System.Windows.Forms
+
+# Function to open the folder browser dialog
+function Select-FolderDialog {
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderBrowser.ShowDialog() | Out-Null
+    return $folderBrowser.SelectedPath
+}
+
+# Function to open the file open dialog
+function Select-FileDialog {
+    $fileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    $fileBrowser.Filter = "Model files (*.pt)|*.pt"
+    $fileBrowser.ShowDialog() | Out-Null
+    return $fileBrowser.FileName
+}
+
+# Output directory for results
+$resultsDir = "$PWD/runs/detect/predict"
+
+# Check if the results directory exists, if not, create it
+if (-Not (Test-Path -Path $resultsDir)) {
+    New-Item -ItemType Directory -Path $resultsDir
+}
+
+# Select the model path
+$modelPath = Select-FileDialog
+if (-not $modelPath) {
+    Write-Output "Model path selection cancelled."
+    exit
+}
+
+# Select the source directory
+$sourceDir = Select-FolderDialog
+if (-not $sourceDir) {
+    Write-Output "Source directory selection cancelled."
+    exit
+}
+
+# Install necessary tools and packages
 winget install --id Git.Git -e --source winget
-# Install necessary python packages
 pip install -q git+https://github.com/THU-MIG/yolov10.git
 pip install -q supervision roboflow
-
-# Download the YOLO model from hugging face
-$HUGGINGFACE_MODEL_URL = 'https://huggingface.co/dreamingdodo/YOLOv10-groceries/resolve/main/best.pt'
-$localModelPath = "C:/Users/balme/Downloads/best.pt"
-Invoke-WebRequest -Uri $HUGGINGFACE_MODEL_URL -OutFile $localModelPath
-
-# Define the source directory for images
-$sourceDir = "C:/Users/balme/Downloads/groceries-images"
 
 # Download images from GitHub repository recursively
 git clone --depth 1 https://github.com/aleksandar-aleksandrov/groceries-object-detection-dataset $sourceDir
 
 # Run YOLO detection on each image in the source directory recursively
-$imageFormats = "*.jpg,*.png,*.jpeg"
-Get-ChildItem -Path $sourceDir -Recurse -Include $imageFormats | ForEach-Object {
+$imageFormats = @(".jpg", ".png", ".jpeg")
+Get-ChildItem -Path $sourceDir -Recurse -Filter "*.png" | ForEach-Object {
     $imagePath = $_.FullName
-    yolo task=detect mode=predict conf=0.25 save=True model=$localModelPath source=$imagePath
+    yolo task=detect mode=predict conf=0.25 save=True model=$modelPath source=$imagePath
 }
 
-# Output directory for results
-$resultsDir = "$PWD/runs/detect/predict"
-Write-Output "Results saved to $resultsDir"
+Write-Output "Results saved to $resultsDir" 
